4e772d1406a738c18ccd91d67f907df95f3ac092
Aaditya Chhatraliya <aaditya.chhatraliya@gmail.com>
2025-07-12 19:48:14 +0530

    First Commit

---
+++ config/db.js
@@ -0,0 +1,29 @@
+import mongoose, { connect } from "mongoose";
+
+let cached = global.mongoose
+
+if (! cached) {
+    cached = global.mongoose = { conn: null, Promise: null }
+}
+
+async function connection() {
+    
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if (!cached.Promise) {
+        const opts = {
+            bufferCommands:false
+        }
+
+        cached.Promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then( mongoose => {
+            return mongoose
+        })
+
+        cached.conn = await cached.Promise
+        return cached.conn
+    }
+}
+
+export default connectdb
