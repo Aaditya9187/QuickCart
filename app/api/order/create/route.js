45ca5e5009944cd69e949703a3cc96bdf72197a0
MD. SAJIB MIAH <mdsojibmiah711@gmail.com>
2026-02-06 18:33:37 +0600

    order price added

---
+++ app/api/order/create/route.js
@@ -0,0 +1,45 @@
+import { inngest } from "@/config/inngest";
+import Product from "@/models/Product";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request) {
+    try{
+        const {userId} = getAuth(request);
+        const {address, items} = await request.json();
+
+        if(!address ||  items.length === 0){
+            return NextResponse.json({success: false, message: "Address and items are required"})
+        }
+
+        //Calculate total amount
+        const amount = await items.reduce(async (acc, item)=>{
+            const product = await Product.findById(item.product);
+            return acc + product.offerPrice * item.quantity;
+        }, 0)
+
+        await inngest.send({
+            name: 'order/created',
+            data:{
+                userId,
+                address,
+                items,
+                amount: amount + Math.floor(amount * 0.02),
+                date: Date.now() 
+            }
+        })
+
+        // Clear user cart after order creation
+        const user = await User.findById(userId);
+        user.cart = [];
+        await user.save();
+
+        return NextResponse.json({success: true, message: "Order Placed"})
+
+    }catch(err){
+        console.log(err);
+        return NextResponse.json({success: false, message: err.message})
+    }
+}
