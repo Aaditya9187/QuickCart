562ba7d91497ebdf717331abdcdbbf750ba78821
MD. SAJIB MIAH <mdsojibmiah711@gmail.com>
2026-02-08 22:52:31 +0600

    seller order page data solve

--- app/seller/orders/page.jsx
+++ app/seller/orders/page.jsx
@@ -53,9 +53,15 @@ const Orders = () => {
                                 />
                                 <p className="flex flex-col gap-3">
                                     <span className="font-medium">
-                                        {order.items.map((item) => item.product.name + ` x ${item.quantity}`).join(", ")}
+                                        {order.items
+                                            .map((item) => {
+                                                if (!item.product) return `${item.quantity}`; // fixed
+                                                return `${item.product.name} x ${item.quantity}`;
+                                            })
+                                            .join(", ")
+                                        }
                                     </span>
-                                    <span>Items : {order.items.length}</span>
+                                    {/* <span>Items : {order.items.length}</span> */}
                                 </p>
                             </div>
                             <div>
