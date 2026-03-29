fdb7c527a4b9d8bd4e1a8b091afefbb07946e211
Aaditya Chhatraliya <aaditya.chhatraliya@gmail.com>
2026-03-30 00:45:22 +0530

    Refactor import paths to use relative imports for consistency in product and inngest files

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -1,4 +1,4 @@
-import Product from "@/models/Product";
+import Product from "../../../../models/Product";
 import connectDB from "../../../../config/db";
 import { NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
