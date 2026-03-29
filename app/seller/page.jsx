149c898d4a7cd747c41248106098dd3c4c11fa00
Aaditya Chhatraliya <aaditya.chhatraliya@gmail.com>
2026-03-29 23:43:05 +0530

    Refactor import paths to use absolute imports for consistency

--- app/seller/page.jsx
+++ app/seller/page.jsx
@@ -2,9 +2,9 @@
 import React, { useState, useEffect } from "react";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { assets } from "../../assets/assets";
+import { assets } from "@/assets/assets";
 import Image from "next/image";
-import { useAppContext } from "../../context/AppContext";
+import { useAppContext } from "@/context/AppContext";
 import toast from "react-hot-toast";
 import axios from "axios";
 
